4445c1ca0e2c22a54a195ca861820d12307e4c6c
Satoru <Satoru_OfficePC@SatoruOfficeMacBook-Pro.local>
2023-11-29 00:22:47 +0900

    fix:server.jsコンフリクト
    Co-authored-by: Takahiro5286 <takahiro5286@users.noreply.github.com>
    Co-authored-by: Satoru-git <satoru-git@users.noreply.github.com>

--- server/src/server.js
+++ server/src/server.js
@@ -1,32 +1,57 @@
 const express = require('express');
 const knex = require('../db');
 const cors = require('cors');
-
 // const testSend = [
-//   {
-//     id: 1,
-//     hotel_name: 'hotelA',
-//     img: '/Users/user/satoru_会社PC/BTC5/Devs/TeamPrg/server/images/ホテル1.jpeg',
-//   },
-//   {
-//     id: 2,
-//     hotel_name: 'hotelB',
-//     img: '/Users/user/satoru_会社PC/BTC5/Devs/TeamPrg/server/images/ホテル2.jpeg',
-//   },
+// {
+// id: 1,
+// hotel_name: 'hotelA',
+// img: '/Users/user/satoru_会社PC/BTC5/Devs/TeamPrg/server/images/ホテル1.jpeg',
+// },
+// {
+// id: 2,
+// hotel_name: 'hotelB',
+// img: '/Users/user/satoru_会社PC/BTC5/Devs/TeamPrg/server/images/ホテル2.jpeg',
+// },
 // ];
 
 const setUpServer = () => {
   const app = express();
-  app.use(cors());
+
   app.use(express.json());
 
-  //   app.get('/', express.static('client/dist'));
+  // app.get('/', express.static('client/dist'));
 
   app.get('/data', async (req, res) => {
     const customerList = await knex('customer').select();
     res.status(200).send(customerList);
   });
 
+  app.post('/data', async (req, res) => {
+    const rakutenApiKey = 1042996976349696385;
+    const hotelData = await fetch(
+      `https://app.rakuten.co.jp/services/api/Travel/KeywordHotelSearch/20170426?format=json&keyword=岐阜&applicationId=${rakutenApiKey}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        const hotelArr = data.hotels
+          .map((obj) => obj.hotel)
+          .map((obj) => {
+            const hotelBasicInfo = obj[0].hotelBasicInfo;
+            return {
+              hotelName: hotelBasicInfo.hotelName,
+              hotelImageUrl: hotelBasicInfo.hotelImageUrl,
+              YoutubeUrl: `https://www.youtube.com/results?search_query=${hotelBasicInfo.hotelName}%E3%80%80食事`,
+              hotelMapImageUrl: hotelBasicInfo.hotelMapImageUrl,
+              access: hotelBasicInfo.access,
+              latitude: hotelBasicInfo.latitude * 0.0002778,
+              longitude: hotelBasicInfo.longitude * 0.0002778,
+            };
+          });
+
+        return hotelArr;
+      });
+
+    res.status(200).send(hotelData);
     // リクエストされるもの：chekin,people,days
   });
 
