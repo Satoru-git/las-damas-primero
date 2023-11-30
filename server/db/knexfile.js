d7dedcedd2a8a3430aaf729d5d426cd2b2b7f9a8
ryozoo <ryozo_ihara@mail.toyota.co.jp>
2023-11-30 18:46:48 +0900

    modify

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -18,33 +18,12 @@ module.exports = {
     },
   },
 
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user: 'username',
-  //     password: 'password',
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10,
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations',
-  //   },
-  // },
-
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: {
       directory: './migrations',
     },
-
-    // pool: {
-    //   min: 2,
-    //   max: 10
-    // },
 
     seeds: { directory: './seeds' },
   },
