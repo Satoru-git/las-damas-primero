f237f9652a8841ee2e9826aa891bf7cab691940b
ryozoo <ryozo_ihara@mail.toyota.co.jp>
2023-12-01 11:44:36 +0900

    modify:seed_002

---
+++ server/sample.js
@@ -0,0 +1,94 @@
+
+0
+: 
+{hotelName: 'コテージ蒜山の森', hotelNo: 142538, reviewAverage: 5, hotelImageUrl: 'https://img.travel.rakuten.co.jp/share/HOTEL/142538/142538.jpg', YoutubeUrl: 'https://www.youtube.com/results?search_query=コテージ蒜山の森%E3%80%80食事', …}
+1
+: 
+{hotelName: 'あけぼの旅館＜岡山県＞', hotelNo: 39915, reviewAverage: 4.75, hotelImageUrl: 'https://img.travel.rakuten.co.jp/share/HOTEL/39915/39915.jpg', YoutubeUrl: 'https://www.youtube.com/results?search_query=あけぼの旅館＜岡山県＞%E3%80%80食事', …}
+2
+: 
+{hotelName: '登録有形文化財の宿　奥津温泉　名泉鍵湯　奥津荘', hotelNo: 19734, reviewAverage: 4.75, hotelImageUrl: 'https://img.travel.rakuten.co.jp/share/HOTEL/19734/19734.jpg', YoutubeUrl: 'https://www.youtube.com/results?search_query=登録有形文化財の宿　奥津温泉　名泉鍵湯　奥津荘%E3%80%80食事', …}
+3
+: 
+{hotelName: 'ザ・シロヤマテラス津山別邸', hotelNo: 168420, reviewAverage: 4.63, hotelImageUrl: 'https://img.travel.rakuten.co.jp/share/HOTEL/168420/168420.jpg', YoutubeUrl: 'https://www.youtube.com/results?search_query=ザ・シロヤマテラス津山別邸%E3%80%80食事', …}
+4
+: 
+{hotelName: '湯原温泉　湯の蔵　つるや', hotelNo: 15060, reviewAverage: 4.62, hotelImageUrl: 'https://img.travel.rakuten.co.jp/share/HOTEL/15060/15060.jpg', YoutubeUrl: 'https://www.youtube.com/results?search_query=湯原温泉　湯の蔵　つるや%E3%80%80食事', …}
+5
+: 
+{hotelName: '暮らしの宿　てまり「おいとま」', hotelNo: 172441, reviewAverage: 4.6, hotelImageUrl: 'https://img.travel.rakuten.co.jp/share/HOTEL/172441/172441.jpg', YoutubeUrl: 'https://www.youtube.com/results?search_query=暮らしの宿　てまり「おいとま」%E3%80%80食事', …}
+6
+: 
+{hotelName: '美作三湯奥津温泉　池田屋河鹿園', hotelNo: 167354, reviewAverage: 4.56, hotelImageUrl: 'https://img.travel.rakuten.co.jp/share/HOTEL/167354/167354.jpg', YoutubeUrl: 'https://www.youtube.com/results?search_query=美作三湯奥津温泉　池田屋河鹿園%E3%80%80食事', …}
+7
+: 
+{hotelName: 'ホテルエクセル岡山＜後楽園・岡山城前＞', hotelNo: 2098, reviewAverage: 4.48, hotelImageUrl: 'https://img.travel.rakuten.co.jp/share/HOTEL/2098/2098.jpg', YoutubeUrl: 'https://www.youtube.com/results?search_query=ホテルエクセル岡山＜後楽園・岡山城前＞%E3%80%80食事', …}
+8
+: 
+{hotelName: 'ホテルグランヴィア岡山', hotelNo: 822, reviewAverage: 4.45, hotelImageUrl: 'https://img.travel.rakuten.co.jp/share/HOTEL/822/822.jpg', YoutubeUrl: 'https://www.youtube.com/results?search_query=ホテルグランヴィア岡山%E3%80%80食事', …}
+9
+: 
+{hotelName: '民宿　南風荘＜岡山県＞', hotelNo: 67227, reviewAverage: 4.4, hotelImageUrl: 'https://img.travel.rakuten.co.jp/share/HOTEL/67227/67227.jpg', YoutubeUrl: 'https://www.youtube.com/results?search_query=民宿　南風荘＜岡山県＞%E3%80%80食事', …}
+10
+: 
+{hotelName: '津山鶴山ホテル', hotelNo: 6280, reviewAverage: 4.32, hotelImageUrl: 'https://img.travel.rakuten.co.jp/share/HOTEL/6280/6280.jpg', YoutubeUrl: 'https://www.youtube.com/results?search_query=津山鶴山ホテル%E3%80%80食事', …}
+11
+: 
+{hotelName: '湯原温泉　湯快感　花やしき', hotelNo: 56313, reviewAverage: 4.31, hotelImageUrl: 'https://img.travel.rakuten.co.jp/share/HOTEL/56313/56313.jpg', YoutubeUrl: 'https://www.youtube.com/results?search_query=湯原温泉　湯快感　花やしき%E3%80%80食事', …}
+12
+: 
+{hotelName: 'アークホテル岡山　－ルートインホテルズ－', hotelNo: 219, reviewAverage: 4.29, hotelImageUrl: 'https://img.travel.rakuten.co.jp/share/HOTEL/219/219.jpg', YoutubeUrl: 'https://www.youtube.com/results?search_query=アークホテル岡山　－ルートインホテルズ－%E3%80%80食事', …}
+13
+: 
+{hotelName: 'ホテルリマーニ', hotelNo: 38473, reviewAverage: 4.28, hotelImageUrl: 'https://img.travel.rakuten.co.jp/share/HOTEL/38473/38473.jpg', YoutubeUrl: 'https://www.youtube.com/results?search_query=ホテルリマーニ%E3%80%80食事', …}
+14
+: 
+{hotelName: '倉敷国際ホテル', hotelNo: 7584, reviewAverage: 4.24, hotelImageUrl: 'https://img.travel.rakuten.co.jp/share/HOTEL/7584/7584.jpg', YoutubeUrl: 'https://www.youtube.com/results?search_query=倉敷国際ホテル%E3%80%80食事', …}
+15
+: 
+{hotelName: 'ホテルアベストグランデ岡山　なごみの湯', hotelNo: 167859, reviewAverage: 4.22, hotelImageUrl: 'https://img.travel.rakuten.co.jp/share/HOTEL/167859/167859.jpg', YoutubeUrl: 'https://www.youtube.com/results?search_query=ホテルアベストグランデ岡山　なごみの湯%E3%80%80食事', …}
+16
+: 
+{hotelName: 'ホテルルートイン倉敷水島', hotelNo: 172996, reviewAverage: 4.2, hotelImageUrl: 'https://img.travel.rakuten.co.jp/share/HOTEL/172996/172996.jpg', YoutubeUrl: 'https://www.youtube.com/results?search_query=ホテルルートイン倉敷水島%E3%80%80食事', …}
+17
+: 
+{hotelName: '休暇村　奥大山　（旧　大山鏡ヶ成）', hotelNo: 8496, reviewAverage: 4.11, hotelImageUrl: 'https://img.travel.rakuten.co.jp/share/HOTEL/8496/8496.jpg', YoutubeUrl: 'https://www.youtube.com/results?search_query=休暇村　奥大山　（旧　大山鏡ヶ成）%E3%80%80食事', …}
+18
+: 
+{hotelName: '岡山ビューホテル（日本ビューホテルチェーン）', hotelNo: 684, reviewAverage: 4.01, hotelImageUrl: 'https://img.travel.rakuten.co.jp/share/HOTEL/684/684.jpg', YoutubeUrl: 'https://www.youtube.com/results?search_query=岡山ビューホテル（日本ビューホテルチェーン）%E3%80%80食事', …}
+19
+: 
+{hotelName: 'アットステイ', hotelNo: 179177, reviewAverage: 4, hotelImageUrl: 'https://img.travel.rakuten.co.jp/share/HOTEL/179177/179177.jpg', YoutubeUrl: 'https://www.youtube.com/results?search_query=アットステイ%E3%80%80食事', …}
+20
+: 
+{hotelName: '三井ガーデンホテル岡山', hotelNo: 4926, reviewAverage: 3.99, hotelImageUrl: 'https://img.travel.rakuten.co.jp/share/HOTEL/4926/4926.jpg', YoutubeUrl: 'https://www.youtube.com/results?search_query=三井ガーデンホテル岡山%E3%80%80食事', …}
+21
+: 
+{hotelName: 'センチュリオンホテル＆スパ倉敷', hotelNo: 162970, reviewAverage: 3.96, hotelImageUrl: 'https://img.travel.rakuten.co.jp/share/HOTEL/162970/162970.jpg', YoutubeUrl: 'https://www.youtube.com/results?search_query=センチュリオンホテル＆スパ倉敷%E3%80%80食事', …}
+22
+: 
+{hotelName: '湯郷温泉　かつらぎ', hotelNo: 16611, reviewAverage: 3.95, hotelImageUrl: 'https://img.travel.rakuten.co.jp/share/HOTEL/16611/16611.jpg', YoutubeUrl: 'https://www.youtube.com/results?search_query=湯郷温泉　かつらぎ%E3%80%80食事', …}
+23
+: 
+{hotelName: '湯郷温泉　清次郎の湯　ゆのごう館', hotelNo: 50540, reviewAverage: 3.92, hotelImageUrl: 'https://img.travel.rakuten.co.jp/share/HOTEL/50540/50540.jpg', YoutubeUrl: 'https://www.youtube.com/results?search_query=湯郷温泉　清次郎の湯　ゆのごう館%E3%80%80食事', …}
+24
+: 
+{hotelName: '瀬戸大橋温泉　やま幸', hotelNo: 27827, reviewAverage: 3.84, hotelImageUrl: 'https://img.travel.rakuten.co.jp/share/HOTEL/27827/27827.jpg', YoutubeUrl: 'https://www.youtube.com/results?search_query=瀬戸大橋温泉　やま幸%E3%80%80食事', …}
+25
+: 
+{hotelName: '倉敷駅前ユニバーサルホテル（ユニバーサルホテルチェーン）', hotelNo: 130504, reviewAverage: 3.61, hotelImageUrl: 'https://img.travel.rakuten.co.jp/share/HOTEL/130504/130504.jpg', YoutubeUrl: 'https://www.youtube.com/results?search_query=倉敷駅前ユニバーサルホテル（ユニバーサルホテルチェーン）%E3%80%80食事', …}
+26
+: 
+{hotelName: 'さつき荘＜岡山県＞', hotelNo: 56321, reviewAverage: 3.5, hotelImageUrl: 'https://img.travel.rakuten.co.jp/share/HOTEL/56321/56321.jpg', YoutubeUrl: 'https://www.youtube.com/results?search_query=さつき荘＜岡山県＞%E3%80%80食事', …}
+27
+: 
+{hotelName: 'ホテルマイステイズ岡山', hotelNo: 183037, reviewAverage: 3.45, hotelImageUrl: 'https://img.travel.rakuten.co.jp/share/HOTEL/183037/183037.jpg', YoutubeUrl: 'https://www.youtube.com/results?search_query=ホテルマイステイズ岡山%E3%80%80食事', …}
+28
+: 
+{hotelName: '花月旅館＜岡山県＞', hotelNo: 18280, reviewAverage: null, hotelImageUrl: 'https://img.travel.rakuten.co.jp/share/HOTEL/18280/18280.jpg', YoutubeUrl: 'https://www.youtube.com/results?search_query=花月旅館＜岡山県＞%E3%80%80食事', …}
+29
+: 
+{hotelName: '蒜山　コテージ　森の散歩', hotelNo: 41090, reviewAverage: null, hotelImageUrl: 'https://img.travel.rakuten.co.jp/share/HOTEL/41090/41090.jpg', YoutubeUrl: 'https://www.youtube.com/results?search_query=蒜山　コテージ　森の散歩%E3%80%80食事', …}
+length
+: 
+30
