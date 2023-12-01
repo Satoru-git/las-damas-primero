21461148f06fe62b3232710b7137faca7dd5f960
ryozoo <ryozo_ihara@mail.toyota.co.jp>
2023-12-01 12:10:01 +0900

    add:get_api/vi/stay/:username

--- server/src/routes/stay.js
+++ server/src/routes/stay.js
@@ -1,3 +1,18 @@
 const router = require('express').Router();
+const knex = require('../../db/index.js');
+
+router.get('/:username', async (req, res) => {
+  const userName = req.params.username;
+  try {
+    const userHistory = await knex
+      .select('hotel_no')
+      .from('customer')
+      .innerJoin('travel', 'customer.id', 'travel.customer_id')
+      .where({ userName: userName });
+    await res.status(200).send(userHistory);
+  } catch (err) {
+    res.status(404).send(err);
+  }
+});
 
 module.exports = router;
