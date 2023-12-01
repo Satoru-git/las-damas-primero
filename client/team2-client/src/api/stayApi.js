f6ba3198b8d7665487e3218f3274af11f65e40d4
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2023-12-01 23:36:44 +0900

    UPDATE : 予約ボタン、泊まったアイコン、評価点表示
    Co-authored-by: MATSUINAOKO <matsuinaoko@users.noreply.github.com>
    Co-authored-by: Ogata-Kazuyoshi <ogata-kazuyoshi@users.noreply.github.com>

--- client/team2-client/src/api/stayApi.js
+++ client/team2-client/src/api/stayApi.js
@@ -2,6 +2,8 @@ import axiosClient from './axiosClient';
 
 const stayApi = {
   getHistories: (username) => axiosClient.get(`/stay/${username}`),
+  postReserve: (username, sendData) =>
+    axiosClient.post(`/stay/${username}`, sendData),
 };
 
 export default stayApi;
