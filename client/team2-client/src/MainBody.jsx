2b74a3c876c368e038ff0607fcfcd5b45083bb4a
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2023-12-03 02:56:29 +0900

    UPDATE : 宿泊済みを並び替え、MODAL追加、CSS追加
    Co-authored-by: MATSUINAOKO <matsuinaoko@users.noreply.github.com>
    Co-authored-by: Ogata-Kazuyoshi <ogata-kazuyoshi@users.noreply.github.com>

--- client/team2-client/src/MainBody.jsx
+++ client/team2-client/src/MainBody.jsx
@@ -3,47 +3,125 @@ import BedIcon from '@mui/icons-material/Bed';
 import StarIcon from '@mui/icons-material/Star';
 import stayApi from './api/stayApi';
 import { v4 as uuidv4 } from 'uuid';
+import { useState, useEffect } from 'react';
+import Modal from './Modal';
+import WarningAmberIcon from '@mui/icons-material/WarningAmber';
 
-const MainBody = ({ hotelData, username }) => {
-  const postReserve = async (hotel_no, hotel_name) => {
+const MainBody = ({
+  hotelData,
+  username,
+  isStayedBtn,
+  setHotelData,
+  searchPrefecture,
+  setIsStayedBtn,
+}) => {
+  const [renderArray, setRenderArray] = useState([...hotelData]);
+  const [isModal, setIsModal] = useState(false);
+  const [hotelName, setHotelName] = useState('');
+  const [hotelImage, setHotelImage] = useState('');
+
+  const postReserve = async (obj) => {
+    const { hotelName: hotel_name, hotelNo: hotel_no, hotelImageUrl } = obj;
+    setHotelName(hotel_name);
+    setHotelImage(hotelImageUrl);
     const sendData = { hotel_no: hotel_no, hotel_name: hotel_name };
     try {
       const response = await stayApi.postReserve(username, sendData);
-      console.log('response : ', response);
+      // console.log('response : ', response);
+      setIsModal(true);
     } catch (err) {
       console.error(`err : ${err}`);
     }
   };
 
+  useEffect(() => {
+    if (isStayedBtn) {
+      const tempHotel = [...hotelData];
+      const notStayed = tempHotel.filter((elem) => !elem.stayed);
+      const stayed = tempHotel.filter((elem) => elem.stayed);
+      const result = [...notStayed, ...stayed];
+      setRenderArray([...result]);
+    } else {
+      setRenderArray([...hotelData]);
+    }
+  }, [hotelData]);
+
+  useEffect(() => {
+    if (isStayedBtn) {
+      const notStayed = renderArray.filter((elem) => !elem.stayed);
+      const stayed = renderArray.filter((elem) => elem.stayed);
+      const result = [...notStayed, ...stayed];
+      setRenderArray([...result]);
+    } else {
+      setRenderArray([...hotelData]);
+    }
+  }, [isStayedBtn]);
+
   return (
     <main>
+      {isModal && (
+        <Modal
+          setIsModal={setIsModal}
+          hotelName={hotelName}
+          hotelImage={hotelImage}
+          setHotelData={setHotelData}
+          username={username}
+          searchPrefecture={searchPrefecture}
+        />
+      )}
       <ul>
-        {hotelData.map((item, index) => (
-          <div className="hotel_card" key={index}>
+        {renderArray.map((item, index) => (
+          <div
+            className="hotel_card"
+            style={{
+              backgroundColor: item.stayed
+                ? 'rgba(168, 166, 166, 0.516)'
+                : 'rgba(52, 52, 52, 0.541)',
+            }}
+            key={index}
+          >
             <div className="idx">{index + 1}</div>
             {Object.keys(item.hotels).map((key) => {
               switch (key) {
                 case 'hotelName':
                   return (
                     <div className="hotel_name" key={uuidv4()}>
-                      <h2>{item.hotels[key]}</h2>
-                      <StarIcon />
-                      <label htmlFor="">
-                        {item.hotels.reviewAverage.toFixed(2)}
-                      </label>
-                      <div className="reserve_btn">
-                        <button
-                          onClick={() => {
-                            postReserve(
-                              item.hotels.hotelNo,
-                              item.hotels.hotelName
-                            );
+                      <div className="each_hotel_name">
+                        <h2>{item.hotels[key]}</h2>
+                        <div className="star_icon">
+                          <StarIcon className="material_star" />
+                        </div>
+                        <div className="review_point">
+                          <label htmlFor="">
+                            {item.hotels.reviewAverage.toFixed(2)}
+                          </label>
+                        </div>
+                      </div>
+                      <div className="booking_wrap">
+                        <div
+                          style={{
+                            visibility: item.stayed ? 'visible' : 'hidden',
                           }}
                         >
-                          予約
-                        </button>
+                          <WarningAmberIcon className="material_caution" />
+                        </div>
+                        <div className="reserve_btn">
+                          <button
+                            onClick={() => {
+                              postReserve(item.hotels);
+                            }}
+                          >
+                            {item.stayed ? '宿泊済' : '予　約'}
+                          </button>
+                        </div>
+                        <div>
+                          {item.stayed ? (
+                            <BedIcon className="bed_icon" />
+                          ) : (
+                            <BedOutlinedIcon className="bed_icon not_stayed" />
+                          )}
+                        </div>
                       </div>
-                      {item.stayed ? <BedIcon /> : <BedOutlinedIcon />}
                     </div>
                   );
                 case 'hotelImageUrl':
